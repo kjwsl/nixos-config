9e48fc3f4c6633c04ea63c4d9f3407a150e77ce1
kjwsl <larpios@protonmail.com>
2024-08-25 01:25:28 +0900

    update: first commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+	description = "Nixos config flake";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		nixvim = {
+			url = "github:nix-community/nixvim";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+
+	};
+
+
+	outputs = { self, nixpkgs, nixvim, ... }@inputs: 
+	{
+		nixvim.makeNixvimWithModules
+		nixosConfigurations = {  
+			default = nixpkgs.lib.nixosSystem {  
+				specialArgs = {inherit inputs;};  
+				modules = [  
+					./hosts/default/configuration.nix  
+				];  
+			};  
+			workmachine = nixpkgs.lib.nixosSystem {  
+				specialArgs = {inherit inputs;};  
+				modules = [  
+					./hosts/workmachine/configuration.nix  
+				];  
+			};  
+		};  
+	};
+}
---
+++ modules/home-manager/terminal/wezterm.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+{
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+    	
+      
+    '';
+  };
+};
