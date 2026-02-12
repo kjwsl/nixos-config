14a53f5cb82e92b3c1ce73af600fd6726981e5c5
ray <kjwsl@fatherslab.com>
2026-02-13 00:45:08 +0900

    fix: don't manage .local and .cargo as whole directories (conflicts with HM fish completions)

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -21,8 +21,8 @@
     ".justfile".source = ../dotfiles/dot_justfile;
     
     # Directories that need raw file management
-    ".cargo".source = ../dotfiles/dot_cargo;
-    ".local".source = ../dotfiles/dot_local;
+    ".cargo/config.toml".source = ../dotfiles/dot_cargo/config.toml;
+    ".local/bin/start-screenpipe.ps1".source = ../dotfiles/dot_local/bin/executable_start-screenpipe.ps1;
     ".fonts".source = ../dotfiles/dot_fonts;
     ".vim".source = ../dotfiles/dot_vim;
     ".gemini".source = ../dotfiles/dot_gemini;
